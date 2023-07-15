43b794006135c50c2e65e0398441b23c6a8b19db
PramodToleti <pramodraina047@gmail.com>
2023-07-15 22:57:54 +0530

    Added task schema

---
+++ server/models/task.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  dueDate: {
+    type: Date,
+    require: true,
+  },
+  status: {
+    type: String,
+    require: true,
+    enum: ["Pending", "In progress", "Completed"],
+    default: "Pending",
+  },
+  assignedUser: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+})
+
+const Task = mongoose.model("Task", taskSchema)
+
+module.exports = Task
