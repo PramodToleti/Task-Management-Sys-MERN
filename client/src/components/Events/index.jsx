20c50f0eb49ec764dee5455aee5e78e5947ec823
PramodToleti <pramodraina047@gmail.com>
2023-07-16 22:43:29 +0530

    Added Events calendar

--- client/src/components/Events/index.jsx
+++ client/src/components/Events/index.jsx
@@ -1,6 +1,17 @@
 import React from "react"
 import Cookies from "js-cookie"
 import { Navigate } from "react-router-dom"
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule"
 
 import Navbar from "../Navbar"
 
@@ -18,6 +29,11 @@ const Events = () => {
         <div>
           <h1>Events</h1>
         </div>
+        <ScheduleComponent height="500px" selectedDate={new Date()}>
+          <Inject
+            services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+          />
+        </ScheduleComponent>
       </div>
     </div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { registerLicense } from "@syncfusion/ej2-base"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NGaF5cXmdCdkx3QHxbf1xzZFRMYF1bRH5PIiBoS35RdUVkWHlfc3RURmlaUUd2"
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
